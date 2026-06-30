7e30cf8a7184da471a2bf2d20619074943aa924f
Adriel <instrutor.adriel@gmail.com>
2026-06-30 08:16:23 -0300

    Subindo projeto inicial com repository e service implementadas

---
+++ src/main/java/com/example/primeiroAppSpring/controller/UsuarioController.java
@@ -0,0 +1,76 @@
+package com.example.primeiroAppSpring.controller;
+
+import com.example.primeiroAppSpring.model.UsuarioForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UsuarioController {
+    @GetMapping("/cadastro")
+    public String exibirCadastro(Model model){
+        //Criando formulário vazio
+        model.addAttribute("usuarioForm", new UsuarioForm());
+
+        model.addAttribute("tituloPagina", "Cadastro");
+        model.addAttribute("subTituloPagina", "Sistema de Gerenciamento de Estoque da Cozinha");
+
+        return "cadastro";
+    }
+    @PostMapping("/cadastro")
+    public String processarCadastro(@ModelAttribute UsuarioForm form, Model model){
+        if(!form.getSenha().equals(form.getConfirmarSenha())){
+            model.addAttribute("erro","Erro, as senhas não conferem!");
+            return "cadastro";
+        }
+        IO.println("Usuário criado com sucesso!");
+        IO.println(form.getNome()+" "+form.getEmail());
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String exibirLogin(Model model){
+        //Criando formulário vazio
+        model.addAttribute("usuarioForm", new UsuarioForm());
+
+        model.addAttribute("tituloPagina", "Bem-Vindo");
+        model.addAttribute("subTituloPagina", "Sistema de Gerenciamento de Estoque da Cozinha");
+
+        return "login";
+    }
+    @PostMapping("/login")
+    public String processarLogin(@ModelAttribute UsuarioForm form, Model model){
+        if(form.getEmail().endsWith("@df.senac.br")){
+            return "redirect:/";
+        }
+        model.addAttribute("erro","E-mail ou senha inválidos!");
+        return "login";
+    }
+
+    @GetMapping("/alterar-senha")
+    public String exibirAlterarSenha(Model model){
+        //Criando formulário vazio
+        model.addAttribute("usuarioForm", new UsuarioForm());
+
+        model.addAttribute("tituloPagina", "Alterar Senha");
+        model.addAttribute("subTituloPagina", "Informe seus dados para alterar a senha");
+
+        return "alterarSenha";
+    }
+    @PostMapping("/alterar-senha")
+    public String processarAlterarSenha(@ModelAttribute UsuarioForm form, Model model){
+        if(!form.getSenha().equals(form.getConfirmarSenha())){
+            model.addAttribute("erro","Erro, as senhas não conferem!");
+            return "alterarSenha";
+        }
+        model.addAttribute("sucesso","Senha alterada com sucesso!");
+
+        return "redirect:/login";
+    }
+
+}
---
+++ src/main/java/com/example/primeiroAppSpring/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.primeiroAppSpring.repository;
+
+import com.example.primeiroAppSpring.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existByEmail(String email);
+}
